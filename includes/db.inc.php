ce59cc2fa7346b141ddaa0944a8c35909ddb617e
BBestamiS <technosor202@outlook.com>
2021-10-04 15:31:27 +0300

    Project template created

---
+++ includes/db.inc.php
@@ -0,0 +1,30 @@
+<!-- Veri tabanı ile bağlantı kodları yazılacak. -->
+<?php
+require_once 'config.php';
+class DBController
+{
+    private $conn = null;
+
+    public function __construct()
+    {
+        $this->conn = mysqli_connect(Config::$serverName, Config::$dbUserName, Config::$dbPassword, Config::$dbName);
+        if ($this->conn->connect_error) {
+            echo "Fail" . $this->conn->connect_error;
+        }
+    }
+    public function get_conn()
+    {
+        return $this->conn;
+    }
+    public function __destruct()
+    {
+        $this->closeConnection();
+    }
+    protected function closeConnection()
+    {
+        if ($this->conn != null) {
+            $this->conn->close();
+            $this->conn = null;
+        }
+    }
+}
